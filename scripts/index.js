a98b9dcf4da6efb7ac840b207eeb2734ec1cd768
Tayse Rosa <tayse_rosa@yahoo.com.br>
2020-06-12 15:24:46 -0300

    Commitando project Javascript Management User by HCode Courses

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked")
+var birth = document.querySelector("#exampleInputBirth")
+var country = document.querySelector("#exampleInputCountry")
+var email = document.querySelector("#exampleInputEmail1")
+var password = document.querySelector("#exampleInputPassword1")
+var file = document.querySelector("#exampleInputFile")
+var admin = document.querySelector("#exampleInputAdmin")
+
